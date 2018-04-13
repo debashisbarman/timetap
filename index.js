ce95ba1c7dda4ffdf4e267b268092bae4ee62f01
Debashis Barman <debashisb04@gmail.com>
2018-04-13 13:00:07 +0530

    Fixed server

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import app from './config/express';
 const host = config.get('server.host');
 const port = config.get('server.port');
 
-app.listen(process.env.PORT || port, host, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is running at http://${host}:${port}`);
 });

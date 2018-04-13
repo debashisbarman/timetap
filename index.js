f7e9a1f694762100cb965c28e14371debfbc8875
Debashis Barman <debashisb04@gmail.com>
2018-04-13 12:35:19 +0530

    Fixed port

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const host = config.get('server.host');
 const port = config.get('server.port');
 
 const server = http.createServer(app);
-server.listen(port, host, () => {
+server.listen(process.env.PORT || port, host, () => {
   console.log(`Server is running at http://${host}:${port}`);
 });
